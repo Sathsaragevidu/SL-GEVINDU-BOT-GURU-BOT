78d7a55acb9d61137d01c68c5dabcab90553b155
Sathsaragevidu <134496144+Sathsaragevidu@users.noreply.github.com>
2023-07-01 00:27:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94761194767', 'owner GEVINDU', true],
+  ['94786926392', 'SL GEVIYA BOT', true], 
   [''] 
 ] //Number of owners
 
